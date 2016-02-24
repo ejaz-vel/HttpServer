456f3eb35f7912dd09320d6fef09a1fba92e26d7
Ejaz Veljee <eveljee@andrew.cmu.edu>
2016-02-24 01:14:33 -0500

    Populating all response fields

--- src/cmu/webserver/response/ResponseHandler.java
+++ src/cmu/webserver/response/ResponseHandler.java
@@ -10,23 +10,24 @@ import cmu.webserver.model.HTTPResponse;
 import cmu.webserver.model.HTTPResponseCode;
 import cmu.webserver.model.HTTPVersion;
 import cmu.webserver.socket.GetMime;
+import net.sourceforge.sizeof.SizeOf;
 
 public class ResponseHandler {
 
 	private static final String SERVER_NAME = "Simple/1.0";
-	
+
 	public HTTPResponse handleRequest(HTTPRequestDetails request) throws IOException {
 		HTTPResponse response = new HTTPResponse();
 		response.setVersion(HTTPVersion.HTTP1_0);
 		response.addField("Server", SERVER_NAME);
 		String fileName = request.getRelativePath() + request.getWebPage();
-		
+
 		if (!request.getMethod().equals(HTTPMethod.GET)
 				&& !request.getMethod().equals(HTTPMethod.HEAD)) {
 			response.setResponseCode(HTTPResponseCode.HTTP_400);
 			return response;
 		}
-		
+
 		try {
 			response.addField("Content-Type", GetMime.getMimeType(fileName));
 			if (request.getMethod().equals(HTTPMethod.GET)) {
@@ -41,11 +42,15 @@ public class ResponseHandler {
 				response.setBody(sb.toString());
 			}
 			response.setResponseCode(HTTPResponseCode.HTTP_200);
+			response.addField("Connection", "close");
+			SizeOf.skipStaticField(true);
+			SizeOf.skipFinalField(true);
+			response.addField("Content-Length",  SizeOf.humanReadable(SizeOf.deepSizeOf(response)));
 		} catch (IOException e) {
 			response.setResponseCode(HTTPResponseCode.HTTP_404);
 			e.printStackTrace();
 		}
-		
+
 		return response;
 	}
 
